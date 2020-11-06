531764114c53fe280d3f9b39d3d5a56cd7128c94
Jose M. Recio <1330220+josemrecio@users.noreply.github.com>
2020-11-06 13:06:35 +0800

    Add option to start without HTTPS, by default uses HTTPS

--- README.md
+++ README.md
@@ -4,19 +4,25 @@ This proxy will forward all requests to it to opentok.com or tokbox.com
 
 If you do a request to `https://PROXY_IP_ADDRESS/tokbox.com/foo/bar?a=b`, It will forward it to `https://tokbox.com/foo/bar?a=b`
 
-By default, uses key and cerfificate from selfsigned.key and selfsigned.crt files
-
 ## Running
+
+### Starting on HTTPS
+
+By default, uses key and cerfificate from selfsigned.key and selfsigned.crt files
 
 ```
 $ npm install
 $ npm start
 ```
 
+### Starting on HTTP
+
+```
+$ npm install
+$ npm run start-no-ssl
+```
+
 ### Create self-signed certificates
 ```
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./selfsigned.key -out selfsigned.crt
 ```
-
-
-
--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const proxy = require('http-proxy-middleware');
 const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
+const http = require('http');
 const https = require('https');
 
 const processUrl = (originalUrl) => {
@@ -33,18 +34,28 @@ const otProxy = proxy({
 
 const app = express()
 const PORT = process.env.PORT || 3000;
+const no_ssl = (
+  process.argv.indexOf('-no-ssl') > -1? true : false
+);
+
 app.use('/', cors(), otProxy);
 app.use('/proxy', cors(), otProxy);
-//const server = app.listen(PORT, () => console.log(`Reverse proxy started.`))
-//process.once('SIGINT', () => { server.close(); });
+let server;
 
-const key = fs.readFileSync(path.join(__dirname, 'selfsigned.key'));
-const cert = fs.readFileSync(path.join(__dirname, 'selfsigned.crt'));
-const options = {
-  key: key,
-  cert: cert
-};
-const httpsServer = https.createServer(options, app);
-httpsServer.listen(PORT, () => console.log(`Reverse proxy started.`))
-process.once('SIGINT', () => { httpsServer.close(); });
+if (no_ssl) {
+  console.log("Starting over HTTP");
+  server = http.createServer(app);
+}
+else {
+  console.log("Starting over HTTPS");
+  const key = fs.readFileSync(path.join(__dirname, 'selfsigned.key'));
+  const cert = fs.readFileSync(path.join(__dirname, 'selfsigned.crt'));
+  const options = {
+    key: key,
+    cert: cert
+  };
+  server = https.createServer(options, app);
+}
+server.listen(PORT, () => console.log(`Reverse proxy started.`))
+process.once('SIGINT', () => { server.close(); });
 
